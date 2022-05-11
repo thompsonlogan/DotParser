893cba880f151a61674887db542a1376994927cc
thompsonlogan <45924391+thompsonlogan@users.noreply.github.com>
2022-05-11 17:54:09 -0500

    Update and rename prog4.rb to main.rb

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require_relative "dot_lexer"
 require_relative "dot_parser"
 
 # Uncomment this line for debugging.
-# $stdin.reopen("Prog4-s1.in")
+# $stdin.reopen("s1.in")
 
 lexer = DotLexer.new
 
